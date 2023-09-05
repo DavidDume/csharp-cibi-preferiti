b2758e1f7be0a4d92de8c9523240eebf9fef23c3
David Dume <dumedavid11@gmail.com>
2023-09-05 15:32:15 +0200

    Stampare informazioni su schermo

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+string[] cibi = { "pizza", "hamburger", "gelato", "bistecca", "patatine" };
+
+Console.WriteLine("Lunghezza classifica" + cibi.Length);
+
+for(int i = 0; i < cibi.Length; i++)
+{
+    Console.WriteLine(i + " " + cibi[i]);
+}
+
+Console.WriteLine("Primo Posto - " + cibi[0]);
+
+Console.WriteLine("Ultimo Posto - " + cibi[cibi.Length - 1]);
