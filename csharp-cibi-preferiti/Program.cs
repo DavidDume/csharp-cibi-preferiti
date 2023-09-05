ed0458eac51e805bd5445c80bfffa476aa50ae44
David Dume <dumedavid11@gmail.com>
2023-09-05 16:05:12 +0200

    piccoli bugfix

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -1,20 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 string[] cibi = { "pizza", "hamburger", "gelato", "bistecca", "patatine" };
 
-Console.WriteLine("Lunghezza classifica" + cibi.Length);
+Console.WriteLine($"Lunghezza classifica {cibi.Length}");
 
-for(int i = 0; i < cibi.Length; i++)
+for(int i = 1; i <= cibi.Length - 1; i++)
 {
-    Console.WriteLine(i + " " + cibi[i]);
+    Console.WriteLine($"{i} - {cibi[i]} ");
 }
 
-Console.WriteLine("Primo Posto - " + cibi[0]);
+Console.WriteLine($"Primo Posto - {cibi[0]}");
 
-Console.WriteLine("Ultimo Posto - " + cibi[cibi.Length - 1]);
+Console.WriteLine($"Ultimo Posto - {cibi[cibi.Length - 1]}");
 
 if (cibi.Length % 2 == 0)
 {
-    Console.WriteLine(cibi[(cibi.Length / 2) - 1] + " " + cibi[cibi.Length / 2]);
+    Console.WriteLine($"{cibi[(cibi.Length / 2) - 1]} - {cibi[cibi.Length / 2]}");
 } else
 {
     Console.WriteLine(cibi[cibi.Length / 2]);
